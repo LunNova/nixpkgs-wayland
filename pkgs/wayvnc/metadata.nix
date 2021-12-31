c391cb353ea1a5a925c4b67d659c6dd8d2ffc87d
Luna Nova <git@lunnova.dev>
2021-12-31 15:04:32 -0800

    auto-update(manual): wayvnc: 8a65a8557eea4f0f1211e8ad575f13db8c410581 => ead9cdf12147f12bcbaf7ab3730c02e1ff8651f6

--- pkgs/wayvnc/metadata.nix
+++ pkgs/wayvnc/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/any1/wayvnc";
   branch = "master";
-  rev = "8a65a8557eea4f0f1211e8ad575f13db8c410581";
-  sha256 = "sha256-Z0h1L+Oz1HhWmJh9COaCET6I2a3qRbm8wGscob1rO0k=";
+  rev = "ead9cdf12147f12bcbaf7ab3730c02e1ff8651f6";
+  sha256 = "sha256-A8rzLQnsSg3EGolo9FRETg0anE6d4f4c71XtNFuyK7I=";
 }
