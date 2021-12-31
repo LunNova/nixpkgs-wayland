ef34b87ff7771359c52461fc52b4b460b8c6012d
Luna Nova <git@lunnova.dev>
2021-12-31 14:47:00 -0800

    auto-update(manual): dunst: 8855f77291e7e6ece59afdcbb6dca578a2718032 => c7358148edef23e883586cca37c0c7ee4b363ce8

--- pkgs/dunst/metadata.nix
+++ pkgs/dunst/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/dunst-project/dunst";
   branch = "master";
-  rev = "8855f77291e7e6ece59afdcbb6dca578a2718032";
-  sha256 = "sha256-B/6bn6FxSMz7y13c9yhVa3keH0OJ5jtEqn1fupGDHw4=";
+  rev = "c7358148edef23e883586cca37c0c7ee4b363ce8";
+  sha256 = "sha256-eZoIeLERDbXIBBm/j9jgqvvul2h0YNjzedbnQGMxsiU=";
 }
